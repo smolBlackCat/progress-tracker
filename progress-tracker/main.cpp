0b0b7dc85549a2b6ae332bd0141f65ff10d96287
moura <m0ur4@protonmail.com>
2023-02-13 12:14:49 -0300

    fix: Fix typo in the CmakeLists.txt where the c++ standard version was not set.

--- progress-tracker/main.cpp
+++ progress-tracker/main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <gtkmm-4.0/gtkmm/application.h>
-#include <gtkmm-4.0/gtkmm/window.h>
+#include <gtkmm/application.h>
+#include <gtkmm/window.h>
 #include "app_info.h"
 
-class MyWindow : public Gtk::Window {
+class MyWindow : public Gtk::Window
+{
 public:
-	MyWindow();
+	MyWindow()
+	{
+		set_title("Progress Tracker");
+		set_default_size(200, 200);
+	};
 };
 
-MyWindow::MyWindow() {
-	set_title("Progress Tracker");
-	set_default_size(200, 200);
-}
-
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 	std::cout << "Progress Tracker " << MAJOR_VERSION << "." << MINOR_VERSION << std::endl;
 	auto app = Gtk::Application::create("com.moura.progress-tracker");
 	return app->make_window_and_run<MyWindow>(argc, argv);
