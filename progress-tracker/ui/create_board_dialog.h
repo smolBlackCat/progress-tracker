b215ad850d470bb12e793e5b3252cb5576524e38
moura <smolBlackCat@proton.me>
2023-04-25 11:46:17 -0300

    add: add CreateBoardDialog documentation.

--- progress-tracker/ui/create_board_dialog.h
+++ progress-tracker/ui/create_board_dialog.h
@@ -19,16 +19,62 @@
 
 namespace ui {
 
+/**
+ * @class CreateBoardDialog
+ *
+ * @brief Class representing a custom dialog window for getting the board's name
+ * and the board's background.
+ */
 class CreateBoardDialog : public Gtk::Window {
    public:
+    /**
+     * @brief Dialog Window constructor
+     */
     CreateBoardDialog();
     std::map<std::string, std::string> get_entry() const noexcept;
 
    private:
+    /**
+     * @brief Reorders selected_bg_label and select_bg_button below the the
+     * solid colour option radiobutton.
+     */
     void on_solid_radiobutton_toggle();
+
+    /**
+     * @brief Reorders selected_bg_label and select_bg_button below the the
+     * image option radiobutton.
+     */
     void on_image_radiobutton_toggle();
+
+    /**
+     * @brief Handles the button click event for the background selection
+     * button.
+     *
+     * This method is a callback function that is triggered when the background
+     * selection button is clicked in the UI. It determines whether the solid
+     * color checkbox is active or not, and opens either a color dialog or a
+     * file dialog accordingly to let the user choose the background for the
+     * board.
+     *
+     * @remarks If the solid color checkbox is active, a color dialog is opened
+     * to let the user choose a color. If the solid color checkbox is not
+     *          active, a file dialog is opened to let the user choose an image
+     * file (JPEG, PNG, or TIFF) as the background for the board.
+     *
+     * @see on_colourdialog_finish()
+     * @see on_filedialog_finish()
+     */
     void on_bg_button_click();
+
+    /**
+     * @brief Stores the entry into a map so the user is able to request it by
+     * using the get_entry() method.
+    */
     void save_entry();
+
+    /**
+     * @brief Closes the dialog window.
+    */
     void close_window();
 
     // Helpers
@@ -36,7 +82,14 @@ class CreateBoardDialog : public Gtk::Window {
     void setup_dialog_root();
 
     // Dialog helpers
+    /**
+     * @brief Callback that stores the selected colour into selected_colour;
+    */
     void on_colourdialog_finish(const Glib::RefPtr<Gio::AsyncResult>& result);
+
+    /**
+     * @brief Callback that stores the selected file into selected_file;
+    */
     void on_filedialog_finish(const Glib::RefPtr<Gio::AsyncResult>& result,
                               const Glib::RefPtr<Gtk::FileDialog>& dialog);
 
